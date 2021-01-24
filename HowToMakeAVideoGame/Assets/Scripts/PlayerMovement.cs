9dd20799c6bcde4ffe94316efc5ea495b8507b41
conawayd <conawayd@nsuok.edu>
2021-01-23 21:24:02 -0600

    GitIgnore Fix
    Fixed GitIgnore to ignore extra files.

---
+++ HowToMakeAVideoGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 100f;
+
+    // Use "Fixed Update" instead of "Update" for physics stuff. Not sure why, but its supposed
+    // to be better than the normal "update".
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
