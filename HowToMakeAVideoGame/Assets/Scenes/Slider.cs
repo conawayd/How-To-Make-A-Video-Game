e1d9c3a7b37e093c8c69c16fbac2cb12518c7148
conawayd <conawayd@nsuok.edu>
2021-01-29 20:08:31 -0600

    Added new stuff

---
+++ HowToMakeAVideoGame/Assets/Scenes/Slider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slider : MonoBehaviour
+{
+    public Text percentageText;
+
+    private void Start()
+    {
+        percentageText = GetComponent<Text>();
+    }
+
+    public void AdjustSlider(float newValue)
+    {
+        percentageText.text = Mathf.RoundToInt(newValue * 100) + "%";
+    }
+}
